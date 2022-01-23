675dc71f96d8eda874831b2255366659d04e0299
Anne <mocchapi@gmail.com>
2022-01-23 23:00:10 +0100

    components, ander spul

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
 
 
 AFRAME.registerComponent("focus",{
-    schema: {text_id: {type:"string"}, on_event: {type:"string", default:"click"},  hide_radius: {type: "int", default: 3}},
+    schema: {text_id: {type:"string"}, on_event: {type:"string", default:"click"},  hide_radius: {type: "int", default: 8}},
     init: function () {
         this.set_textvisible = (state) => {
             this.is_shown = state
-            this.getElementById(this.data.text_id).setAttribute("visible",state);
+            console.log("changed ",this.data.text_id," visibility to ",state)
+            document.getElementById(this.data.text_id).setAttribute("visible",state);
         }
         this.is_shown = false
         this.set_textvisible(false)
         this.el.addEventListener(this.data.on_event, (event) => {
-            
+            this.set_textvisible(true)
         })},
     tick: function() {
         if (this.is_shown) {
             let cam = document.getElementById("main-camera").object3D.position
-            let me = this.object3D.position
+            let me = this.el.object3D.position
             let distance = cam.distanceTo(me)
             if (distance > this.data.hide_radius) {
                 this.set_textvisible(false)
             }
     }
     }
+})
+
+AFRAME.registerComponent('destination', {
+    schema: {},
+    events:{
+        click: function() {
+            let att = document.createAttribute("animation")
+            let cam = document.getElementById("main-camera")
+            mypos = this.el.getAttribute("position")
+            att.value = "property: position; easing: linear; dur: 2000; to: " + mypos.x + " "+ cam.getAttribute("position").y +" " + mypos.z
+            cam.setAttribute("animation", att.value)        }
+    }
+
 })
 
 AFRAME.registerComponent("button", {
